899635a82f09bc74cbaf468e929940ac04a5d277
Coni63 <mine.nicolas@gmail.com>
2017-11-26 20:02:17 +0100

    modification post mentorat

--- P3/test.py
+++ P3/test.py
@@ -11,7 +11,18 @@ def recommander(film_id = 1, nb_movie=5):
     distance = np.sqrt(np.sum(np.square(relative_position), axis=1))
     n_closest = np.argsort(distance)[0:nb_movie+1]
 
+    X = np.load('df_encoded.npz')["X"]
+    center = X[film_id]
+    relative_position = X - center
+    dist = np.sqrt(np.sum(np.square(relative_position), axis=1))
+    distance = np.sqrt(np.sum(np.square(relative_position), axis=1))
+    n_closest_2 = np.argsort(distance)[1:nb_movie+1]
+
+    print("TSNE :")
     print(df.iloc[n_closest][["movie_title", "title_year", "duration", "genres", "country", "imdb_score", "movie_facebook_likes", "director_name"]])
+    print("\nModele Simple :")
+    print(df.iloc[n_closest_2][["movie_title", "title_year", "duration", "genres", "country", "imdb_score", "movie_facebook_likes","director_name"]])
+    print("\n######################################")
 
 if __name__ == "__main__":
     movie_check = [1, 2, 6, 32, 222, 1250, 2500]
