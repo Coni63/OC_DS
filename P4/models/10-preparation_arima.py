d4a8f34edd74e08cf5f441a66ba464bc9eabd03e
Coni63 <mine.nicolas@gmail.com>
2017-11-26 20:02:46 +0100

    test du ARIMA avec evaluation sur le dataset 2017

---
+++ P4/models/10-preparation_arima.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+df = pd.read_csv("../dataset/lighted/2016_01_light.csv")
+
+for i in range(2, 13):
+    print("Load Month:", i)
+    df2 = pd.read_csv("../dataset/lighted/2016_{:02d}_light.csv".format(i))
+    df = df.append(df2)
+
+df.dropna(axis=1, how="all", inplace=True)
+df.drop(["MONTH", "DAY_OF_MONTH", "DAY_OF_WEEK", "UNIQUE_CARRIER", "ORIGIN_AIRPORT_ID", "DEST_AIRPORT_ID", "CRS_ARR_TIME", "ARR_DELAY", "CANCELLED", "DIVERTED", "DISTANCE", "WEATHER_DELAY", "NAS_DELAY", "SECURITY_DELAY", "LATE_AIRCRAFT_DELAY"], axis=1, inplace=True)
+
+df.to_csv("../dataset/arima/arima_dataset.csv", index=False)
