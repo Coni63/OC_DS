7720b46c747452c12c044fb76633f04272874c78
Coni63 <mine.nicolas@gmail.com>
2018-01-25 20:47:44 +0100

    modification de l'architecture

---
+++ P9/models/model.py
@@ -0,0 +1,176 @@
+import numpy as np
+import pickle
+import imageio
+import matplotlib.pyplot as plt
+
+from keras.models import Model, load_model
+from keras.layers import Input
+from keras.layers.convolutional import Conv2D, Conv2DTranspose
+from keras.layers.pooling import MaxPooling2D
+from keras.layers.merge import concatenate
+from keras.callbacks import EarlyStopping, ModelCheckpoint
+from keras import backend as K
+from keras.preprocessing.image import ImageDataGenerator
+from keras.callbacks import Callback
+from keras.optimizers import Adam
+from keras import regularizers
+
+import tensorflow as tf
+
+def save_obj(obj, name):
+    with open(name + '.pkl', 'wb') as f:
+        pickle.dump(obj, f, pickle.HIGHEST_PROTOCOL)
+
+class prediction_history(Callback):
+    def __init__(self, model, X_test, y_test, folder):
+        self.img_id = [8, 11, 12, 24]
+        self.epoch = 0
+        self.val_loss = []
+        self.loss = []
+        self.X_test = X_test
+        self.y_test = y_test
+        self.model = model
+        self.folder = folder
+        for idx in self.img_id:
+            self.generate_image(X_test[idx, :, :, 0], "Init", idx)
+            self.generate_image(y_test[idx, :, :, 0], "Mask", idx)
+
+    def on_epoch_end(self, epoch, logs={}, model=None):
+        self.epoch += 1
+        for idx in self.img_id:
+            y_pred = self.model.predict(np.expand_dims(self.X_test[idx], 0))
+            self.generate_image(y_pred[0, :, :, 0], "Epoch", idx)
+        self.val_loss.append(logs.get('val_loss'))
+        self.loss.append(logs.get('loss'))
+
+    def generate_image(self, img, name="Epoch", index=0):
+        plt.imshow(img, cmap='gray')
+        plt.title("Epoch {}".format(self.epoch))
+        plt.savefig("img/training/{}/{}/{}{:03d}.png".format(self.folder, index, name, self.epoch))
+
+    def on_train_end(self, logs={}):
+        result = {"val_loss": self.val_loss, "loss": self.loss}
+        save_obj(result, "datas/results1D")
+        for img_id in self.img_id:
+            images = []
+            for i in range(1, self.epoch):
+                images.append(imageio.imread("img/training/{}/{}/Epoch{:03d}.png".format(self.folder, img_id, i)))
+            output_file = 'img/training1D-{}.gif'.format(img_id)
+            imageio.mimsave(output_file, images, duration=0.5)
+
+
+def save_obj(obj, name):
+    with open(name + '.pkl', 'wb') as f:
+        pickle.dump(obj, f, pickle.HIGHEST_PROTOCOL)
+
+def create_model(IMG_HEIGHT, IMG_WIDTH, IMG_CHANNELS):
+    K_REG = None #regularizers.l1(0.1)
+    BIAS_INIT = "zeros" # "he_normal"
+    KERNEL_INIT = "glorot_uniform" # "he_normal"
+
+    inputs = Input((IMG_HEIGHT, IMG_WIDTH, IMG_CHANNELS))
+    # s = Lambda(lambda x: x / 255) (inputs)
+
+    c1 = Conv2D(8, (3, 3), activation='relu', padding='same', kernel_initializer=KERNEL_INIT, bias_initializer = BIAS_INIT, kernel_regularizer=K_REG) (inputs)
+    c1 = Conv2D(8, (3, 3), activation='relu', padding='same', kernel_initializer=KERNEL_INIT, bias_initializer = BIAS_INIT, kernel_regularizer=K_REG) (c1)
+    p1 = MaxPooling2D((2, 2)) (c1)
+
+    c2 = Conv2D(16, (3, 3), activation='relu', padding='same', kernel_initializer=KERNEL_INIT, bias_initializer = BIAS_INIT, kernel_regularizer=K_REG) (p1)
+    c2 = Conv2D(16, (3, 3), activation='relu', padding='same', kernel_initializer=KERNEL_INIT, bias_initializer = BIAS_INIT, kernel_regularizer=K_REG) (c2)
+    p2 = MaxPooling2D((2, 2)) (c2)
+
+    c3 = Conv2D(32, (3, 3), activation='relu', padding='same', kernel_initializer=KERNEL_INIT, bias_initializer = BIAS_INIT, kernel_regularizer=K_REG) (p2)
+    c3 = Conv2D(32, (3, 3), activation='relu', padding='same', kernel_initializer=KERNEL_INIT, bias_initializer = BIAS_INIT, kernel_regularizer=K_REG) (c3)
+    p3 = MaxPooling2D((2, 2)) (c3)
+
+    c4 = Conv2D(64, (3, 3), activation='relu', padding='same', kernel_initializer=KERNEL_INIT, bias_initializer = BIAS_INIT, kernel_regularizer=K_REG) (p3)
+    c4 = Conv2D(64, (3, 3), activation='relu', padding='same', kernel_initializer=KERNEL_INIT, bias_initializer = BIAS_INIT, kernel_regularizer=K_REG) (c4)
+    p4 = MaxPooling2D(pool_size=(2, 2)) (c4)
+
+    c5 = Conv2D(128, (3, 3), activation='relu', padding='same', kernel_initializer=KERNEL_INIT, bias_initializer = BIAS_INIT, kernel_regularizer=K_REG) (p4)
+    c5 = Conv2D(128, (3, 3), activation='relu', padding='same', kernel_initializer=KERNEL_INIT, bias_initializer = BIAS_INIT, kernel_regularizer=K_REG) (c5)
+
+    u6 = Conv2DTranspose(64, (2, 2), strides=(2, 2), padding='same', kernel_initializer=KERNEL_INIT, bias_initializer = BIAS_INIT, kernel_regularizer=K_REG) (c5)
+    u6 = concatenate([u6, c4])
+    c6 = Conv2D(64, (3, 3), activation='relu', padding='same', kernel_initializer=KERNEL_INIT, bias_initializer = BIAS_INIT, kernel_regularizer=K_REG) (u6)
+    c6 = Conv2D(64, (3, 3), activation='relu', padding='same', kernel_initializer=KERNEL_INIT, bias_initializer = BIAS_INIT, kernel_regularizer=K_REG) (c6)
+
+    u7 = Conv2DTranspose(32, (2, 2), strides=(2, 2), padding='same', kernel_initializer=KERNEL_INIT, bias_initializer = BIAS_INIT, kernel_regularizer=K_REG) (c6)
+    u7 = concatenate([u7, c3])
+    c7 = Conv2D(32, (3, 3), activation='relu', padding='same', kernel_initializer=KERNEL_INIT, bias_initializer = BIAS_INIT, kernel_regularizer=K_REG) (u7)
+    c7 = Conv2D(32, (3, 3), activation='relu', padding='same', kernel_initializer=KERNEL_INIT, bias_initializer = BIAS_INIT, kernel_regularizer=K_REG) (c7)
+
+    u8 = Conv2DTranspose(16, (2, 2), strides=(2, 2), padding='same', kernel_initializer=KERNEL_INIT, bias_initializer = BIAS_INIT, kernel_regularizer=K_REG) (c7)
+    u8 = concatenate([u8, c2])
+    c8 = Conv2D(16, (3, 3), activation='relu', padding='same', kernel_initializer=KERNEL_INIT, bias_initializer = BIAS_INIT, kernel_regularizer=K_REG) (u8)
+    c8 = Conv2D(16, (3, 3), activation='relu', padding='same', kernel_initializer=KERNEL_INIT, bias_initializer = BIAS_INIT, kernel_regularizer=K_REG) (c8)
+
+    u9 = Conv2DTranspose(8, (2, 2), strides=(2, 2), padding='same', kernel_initializer=KERNEL_INIT, bias_initializer = BIAS_INIT, kernel_regularizer=K_REG) (c8)
+    u9 = concatenate([u9, c1], axis=3)
+    c9 = Conv2D(8, (3, 3), activation='relu', padding='same', kernel_initializer=KERNEL_INIT, bias_initializer = BIAS_INIT, kernel_regularizer=K_REG) (u9)
+    c9 = Conv2D(8, (3, 3), activation='relu', padding='same', kernel_initializer=KERNEL_INIT, bias_initializer = BIAS_INIT, kernel_regularizer=K_REG) (c9)
+
+    outputs = Conv2D(1, (1, 1), activation='sigmoid') (c9)
+
+    model = Model(inputs=[inputs], outputs=[outputs])
+
+    model.compile(optimizer = "adam", loss = 'binary_crossentropy', metrics = ['binary_crossentropy'])
+
+    return model
+
+
+def create_generator(X_train, X_test, y_train, y_test, BATCH_SIZE, seed = 42):
+    data_gen_args_train = {
+        #"rotation_range" : 90.,
+        #"width_shift_range" : 0.1,
+        #"height_shift_range" : 0.1,
+        #"zoom_range" : 0.2,
+        #"shear_range" : 0.2,
+        #"fill_mode" : "nearest",
+        #"cval" : 0,
+        "horizontal_flip" : True,
+        "vertical_flip" : True,
+        "data_format" : "channels_last",
+        "rescale" : 1./255                      # convert uint8 to float32
+    }
+
+    data_gen_args_test = {
+        "horizontal_flip" : False,
+        "vertical_flip" : False,
+        "data_format" : "channels_last",
+        "rescale" : 1./255                      # convert uint8 to float32
+    }
+
+    # Creation of image data augmenter for train set
+    image_datagen_train = ImageDataGenerator(**data_gen_args_train)
+    mask_datagen_train = ImageDataGenerator(**data_gen_args_train)
+
+    # fit this generator to train images
+    image_datagen_train.fit(X_train, augment=True, seed=seed)
+    mask_datagen_train.fit(y_train, augment=True, seed=seed)
+
+    # create a generator of train images augmented
+    image_generator_train = image_datagen_train.flow(X_train, batch_size=BATCH_SIZE, seed=seed)
+    mask_generator_train = mask_datagen_train.flow(y_train, batch_size=BATCH_SIZE, seed=seed)
+
+    # Creation of data augmenter for test set
+    image_datagen_test = ImageDataGenerator(**data_gen_args_test)
+    mask_datagen_test = ImageDataGenerator(**data_gen_args_test)
+
+    # fit this generator to test images
+    image_datagen_test.fit(X_test, augment=True, seed=seed)
+    mask_datagen_test.fit(y_test, augment=True, seed=seed)
+
+    # create a generator of test images augmented
+    image_generator_test = image_datagen_test.flow(X_test, batch_size=BATCH_SIZE, seed=seed)
+    mask_generator_test = mask_datagen_test.flow(y_test, batch_size=BATCH_SIZE, seed=seed)
+
+    # zip to output the X_batch a y_batch
+    train_generator = zip(image_generator_train, mask_generator_train)
+    test_generator = zip(image_generator_test, mask_generator_test)
+
+    return train_generator, test_generator
+
+
+if __name__ == "__main__":
+    print("00")
--- P9/training.py
+++ P9/training.py
@@ -1,108 +1,34 @@
 import numpy as np
-import matplotlib.pyplot as plt
+import pickle
 
-from keras.models import Model, load_model
-from keras.layers import Input
-from keras.layers.core import Lambda
-from keras.layers.convolutional import Conv2D, Conv2DTranspose
-from keras.layers import Cropping2D
-from keras.layers.pooling import MaxPooling2D
-from keras.layers.merge import concatenate
-from keras.callbacks import EarlyStopping, ModelCheckpoint
-from keras import backend as K
-from keras.preprocessing.image import ImageDataGenerator
-from keras.optimizers import *
+from sklearn.model_selection import train_test_split
 
-import tensorflow as tf
+from models import model as md
 
-data = np.load('datas/dataset_RGB.npz')
-X_train = data["X"]
-y_train = data["y"]
+def load_obj(name):
+    with open(name + '.pkl', 'rb') as f:
+        return pickle.load(f)
 
-IMG_HEIGHT, IMG_WIDTH, IMG_CHANNELS = 512, 512, 3
+dimensions = np.array(load_obj("datas/dimensions"))
+data = np.load('datas/dataset_resized.npz')
+X_train, X_test, y_train, y_test, dim_train, dim_test = train_test_split(data["X"], data["y"], dimensions, test_size=0.1, random_state=42)
 
-inputs = Input((IMG_HEIGHT, IMG_WIDTH, IMG_CHANNELS))
-# s = Lambda(lambda x: x / 255) (inputs)
+h, w, c = X_train.shape[1:]
+BATCH_SIZE= 5
+NB_IMAGES_TRAIN = X_train.shape[0]
+NB_IMAGES_TEST = X_test.shape[0]
+EPOCHS = 10
+SEED = 42
 
-c1 = Conv2D(8, (3, 3), activation='relu', padding='same') (inputs)
-c1 = Conv2D(8, (3, 3), activation='relu', padding='same') (c1)
-p1 = MaxPooling2D((2, 2)) (c1)
+model = md.create_model(h, w, c)
+predictions = md.prediction_history(model, X_test, y_test, "resized")  #  1D, 2D, light, resized, rgb
+train_generator, test_generator = md.create_generator(X_train, X_test, y_train, y_test, BATCH_SIZE=BATCH_SIZE, seed=SEED)
 
-c2 = Conv2D(16, (3, 3), activation='relu', padding='same') (p1)
-c2 = Conv2D(16, (3, 3), activation='relu', padding='same') (c2)
-p2 = MaxPooling2D((2, 2)) (c2)
+model.fit_generator(generator = train_generator, 
+                    steps_per_epoch = NB_IMAGES_TRAIN//BATCH_SIZE, 
+                    epochs=EPOCHS, 
+                    validation_data=test_generator,
+                    validation_steps = NB_IMAGES_TEST//BATCH_SIZE, 
+                    callbacks = [predictions])
 
-c3 = Conv2D(32, (3, 3), activation='relu', padding='same') (p2)
-c3 = Conv2D(32, (3, 3), activation='relu', padding='same') (c3)
-p3 = MaxPooling2D((2, 2)) (c3)
-
-c4 = Conv2D(64, (3, 3), activation='relu', padding='same') (p3)
-c4 = Conv2D(64, (3, 3), activation='relu', padding='same') (c4)
-p4 = MaxPooling2D(pool_size=(2, 2)) (c4)
-
-c5 = Conv2D(128, (3, 3), activation='relu', padding='same') (p4)
-c5 = Conv2D(128, (3, 3), activation='relu', padding='same') (c5)
-
-u6 = Conv2DTranspose(64, (2, 2), strides=(2, 2), padding='same') (c5)
-u6 = concatenate([u6, c4])
-c6 = Conv2D(64, (3, 3), activation='relu', padding='same') (u6)
-c6 = Conv2D(64, (3, 3), activation='relu', padding='same') (c6)
-
-u7 = Conv2DTranspose(32, (2, 2), strides=(2, 2), padding='same') (c6)
-u7 = concatenate([u7, c3])
-c7 = Conv2D(32, (3, 3), activation='relu', padding='same') (u7)
-c7 = Conv2D(32, (3, 3), activation='relu', padding='same') (c7)
-
-u8 = Conv2DTranspose(16, (2, 2), strides=(2, 2), padding='same') (c7)
-u8 = concatenate([u8, c2])
-c8 = Conv2D(16, (3, 3), activation='relu', padding='same') (u8)
-c8 = Conv2D(16, (3, 3), activation='relu', padding='same') (c8)
-
-u9 = Conv2DTranspose(8, (2, 2), strides=(2, 2), padding='same') (c8)
-u9 = concatenate([u9, c1], axis=3)
-c9 = Conv2D(8, (3, 3), activation='relu', padding='same') (u9)
-c9 = Conv2D(8, (3, 3), activation='relu', padding='same') (c9)
-
-outputs = Conv2D(1, (1, 1), activation='sigmoid') (c9)
-
-model = Model(inputs=[inputs], outputs=[outputs])
-
-model.compile(optimizer = Adam(lr = 1e-4), loss = 'binary_crossentropy', metrics = ['binary_crossentropy'])
-
-data_gen_args = {
-    "rotation_range" : 90.,
-    "width_shift_range" : 0.1,
-    "height_shift_range" : 0.1,
-    "zoom_range" : 0.2,
-    "shear_range" : 0.2,
-    "fill_mode" : "constant",
-    "cval" : 0,
-    "horizontal_flip" : True,
-    "vertical_flip" : True,
-    "data_format" : "channels_last",
-	"rescale" : 1./255
-}
-
-image_datagen = ImageDataGenerator(**data_gen_args)
-mask_datagen = ImageDataGenerator(**data_gen_args)
-
-seed = 1
-image_datagen.fit(X_train, augment=True, seed=seed)
-mask_datagen.fit(y_train, augment=True, seed=seed)
-
-image_generator = image_datagen.flow(X_train, batch_size=10, seed=seed)
-mask_generator = mask_datagen.flow(y_train, batch_size=10, seed=seed)
-
-train_generator = zip(image_generator, mask_generator)
-
-model.fit_generator(train_generator, steps_per_epoch=67, epochs=5)
-
-image_list= [10, 25, 152, 200, 255]
-f, axarr = plt.subplots(3, len(image_list), figsize=(20,12))
-for idx, img_id in enumerate(image_list):
-    y_pred = model.predict(np.expand_dims(X_train[img_id], 0))
-    axarr[0, idx].imshow(X_train[img_id, :, : , 0].astype(np.float64), cmap='gray')
-    axarr[1, idx].imshow(y_pred[0, :, : , 0], cmap='gray')
-    axarr[2, idx].imshow(y_train[img_id, :, : , 0].astype(np.float64), cmap='gray')
-plt.savefig("result.png")
-#plt.show()
+model.save("models/model.h5")
